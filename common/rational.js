cf3f9a0be30ba63db6b2d35033b0657c537f2b26
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2012-01-20 17:53:33 -0700

    sketched Raffine class

--- common/rational.js
+++ common/rational.js
@@ -57,14 +57,14 @@ var Rational = function (m,n) {
 
 Rational.prototype = {
 
+  /** @returns {string} */
+  toString: function () {
+    return this.numer + '/' + this.denom;
+  },
+
   /** @returns {number} */
   toNumber: function () {
     return this.numer / this.denom;
-  },
-
-  /** @returns {Rational} */
-  recip: function () {
-    return new Rational(this.denom, this.numer);
   },
 
   /** @returns {number} */
@@ -210,23 +210,81 @@ test('Rational.ball of size 88', function(){
  * @param {Rational}
  * @param {Rational}
  */
-var Raffine = function (q,r) {
-
-  // ensure q in [0,1]
-  if (q.numer >= q.denom) {
-    q = new Rational(q.numer % q.denom, q.denom);
+var Raffine = function (r,q) {
+  if (testing) {
+    assert(r.numer > 0 && r.denom > 0, 'invalid step: ' + r);
+    assert(q.denom > 0, 'invalid base: ' + q);
   }
 
-  this.base = q;
+  this.base = q.numer < q.denom ? q : new Rational(q.numer % q.denom, q.denom);
   this.step = r;
+
+  if (testing) {
+    var base = this.base.toNumber();
+    assert(0 <= base && base < 1, 'bad Raffine.base: ' + this.base);
+  }
 };
 
 Raffine.prototype = {
 
+  /** @returns {string} */
+  toString: function () {
+    return this.step + ' * (' + this.base + ' + t)';
+  },
+
   /** @returns {number} */
-  normSquared: function () {
-    TODO('implement Raffine norm');
+  norm: function () {
+    var result = this.step.norm();
+    var base = this.base.toNumber();
+    if (base !== 0) {
+      result /= Math.min(base, 1 - base);
+    }
+    return result;
   }
+};
+
+/**
+ * @const
+ * @type {Raffine}
+ */
+Raffine.UNIT = new Raffine(Rational.ONE, Rational.ZERO);
+
+/**
+ * @param {Raffine}
+ * @param {Raffine}
+ * @returns {boolean}
+ */
+Raffine.equal = function (lhs, rhs) {
+  return ( Rational.cmp(lhs.step, rhs.step) === 0 &&
+           Rational.cmp(lhs.base, rhs.base) === 0 );
+};
+
+/**
+ * mul(r * (q + t), r' * (q' + t))
+ *     = r * (q + (r' * (q' + t)))
+ *     = (r * r') * ((q/r' + q') + t)
+ *
+ * @param {Raffine}
+ * @param {Raffine}
+ * @returns {Raffine}
+ */
+Raffine.mul = function (lhs, rhs) {
+  var step = Rational.mul(lhs.step, rhs.step);
+  var base = Rational.add(Rational.div(lhs.base, rhs.step), rhs.base);
+  return new Raffine(step, base);
+};
+
+/**
+ * @param {Raffine}
+ * @param {Raffine}
+ * @returns {Raffine}
+ */
+Raffine.div = function (lhs, rhs) {
+  var step = Rational.div(lhs.step, rhs.step);
+  var rhs_base = rhs.base;
+  var neg_base = new Rational(rhs_base.denom - rhs_base.numer, rhs_base.denom);
+  var base = Rational.mul(Rational.add(lhs.base, neg_base), rhs.step);
+  return new Raffine(step, base);
 };
 
 /**
@@ -234,6 +292,79 @@ Raffine.prototype = {
  * @returns {Raffine[]}
  */
 Raffine.ball = function (radius) {
-  TODO('implement Raffine.ball');
+
+  var result = [];
+
+  // initialize with unshifted grids
+  var rates = Rational.ball(radius);
+  var I = rates.length;
+  for (var i = 0; i < I; ++i) {
+    result.push(new Raffine(rates[i], Rational.ZERO));
+  }
+
+  // add shifted grids for every pair of unshifted grid
+  for (var i1 = 0; i1 < I; ++i1) {
+    var rate1 = rates[i1];
+    var baseHashSet = {};
+
+    for (var i2 = 0; i2 < I; ++i2) {
+      var rate2 = rates[i2];
+      var relRate = Rational.div(rate2, rate1);
+      var baseDenom = relRate.denom;
+
+      for (var baseNumer = 1; baseNumer < baseDenom; ++baseNumer) {
+        var base = new Rational(baseNumer, baseDenom);
+        var baseHash = base.toString();
+        if (baseHash in baseHashSet) continue;
+
+        var r = new Raffine(rate1, base);
+        if (r.norm() > radius) continue;
+
+        baseHashSet[baseHash] = undefined;
+        result.push(r);
+      }
+    }
+  }
+
+  return result;
 };
+
+test('Raffine.mul, Raffine.div', function(){
+  var x = new Raffine(new Rational(2,3), new Rational(5,7));
+  var y = new Raffine(new Rational(11,13), new Rational(17,19));
+  //var z = new Raffine(new Rational(23,29), new Rational(31,37));
+
+  var unit_x = Raffine.mul(Raffine.UNIT, x);
+  assertEqual(x, unit_x, 'left-unit fails');
+
+  var x_unit = Raffine.mul(x, Raffine.UNIT);
+  assertEqual(x, x_unit, 'right-unit fails');
+
+  var xy = Raffine.mul(x,y);
+  var xyy = Raffine.div(xy,y);
+  assertEqual(x, xyy, 'division fails');
+});
+
+test('Raffine.ball', function(){
+  var radius = 12;
+  var rationalBall = Rational.ball(radius);
+  var raffineBall = Raffine.ball(radius);
+  log('Rational.ball('+radius+').length = '+rationalBall.length);
+  log('Raffine.ball('+radius+').length = '+raffineBall.length);
+
+  assert(rationalBall.length < raffineBall.length,
+      'Raffine.ball is not larger than Rational.ball');
+
+  var I = raffineBall.length;
+  for (var i1 = 0; i1 < I; ++i1) {
+    var r1 = raffineBall[i1];
+    var norm1 = r1.norm();
+    assert(norm1 <= radius, 'norm exceeds radius: |' + r1 + '| = ' + norm1);
+
+    for (var i2 = 0; i2 < i1; ++i2) {
+      var r2 = raffineBall[i2];
+      assert(!Raffine.equal(r1, r2), 'repeated entry: ' + r1);
+    }
+  }
+});
 
